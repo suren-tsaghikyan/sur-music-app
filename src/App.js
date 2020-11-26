3ac05a6fa7b61393a90b7e4996d67755a35a755d
suren-tsaghikyan <stsaghikyan@gmail.com>
2020-11-26 10:19:50 +0400

    firebase

--- src/App.js
+++ src/App.js
@@ -9,22 +9,73 @@ import store from "./store/Reducers";
 import Grid from '@material-ui/core/Grid';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box p={3}>{children}</Box>
+            )}
+        </div>
+    );
+}
+
+function a11yProps(index) {
+    return {
+        id: `simple-tab-${index}`,
+        'aria-controls': `simple-tabpanel-${index}`,
+    };
+}
 
 
 const App = () => {
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
     return (
         <Provider store={store}>
             <Box component="div">
                 <Container>
                     <Header />
-                    <Grid container alignItems="center">
-                        <Grid item lg={8} xs={12}>
-                            <Player />
+                    <AppBar position="static">
+                        <Tabs value={value} onChange={handleChange} centered>
+                        <Tab label="Audios" {...a11yProps(0)} />
+                        <Tab label="Videos" {...a11yProps(1)} />
+                        </Tabs>
+                    </AppBar>
+                    <TabPanel value={value} index={0}>
+                        <Grid container alignItems="center">
+                            <Grid item lg={8} xs={12}>
+                                <Player />
+                            </Grid>
+                            <Grid item lg={4} xs={12}>
+                                <PlayLists />
+                            </Grid>
                         </Grid>
-                        <Grid item lg={4} xs={12}>
-                            <PlayLists />
+                    </TabPanel>
+                    <TabPanel value={value} index={1}>
+                        <Grid container alignItems="center">
+                            <Grid item lg={8} xs={12}>
+                                <Player />
+                            </Grid>
+                            <Grid item lg={4} xs={12}>
+                                <PlayLists />
+                            </Grid>
                         </Grid>
-                    </Grid>
+                    </TabPanel>
                     <Footer />
                 </Container>
             </Box>
